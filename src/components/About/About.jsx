11ab33c381ad3563d1686e1cad3fb4a9b17cd81d
Jose Jimenez <josejimenez826@gmail.com>
2022-03-02 23:04:08 -0800

    update about me and made it responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,13 @@ function About() {
   return (
     <div id="portfolio_about">
         <div id="portfolio_about_shape"></div>
-        <p>A Web Developer with an Associates’s Degree in computer information science at Victor Valley College and a certificate in Web Development from the University of California Riverside. Proficient in programming with HTML, CSS, and javascript which is used to connect both the frontend and backend of a website.</p>
+        <h1>Hello I am Jose Jimenez</h1>
+
+        <p>I am Web Developer with an Associates’s Degree in computer information science at Victor Valley College and a certificate in Web Development from the University of California Riverside. The certificate from the University of California, Riverside was from a 24 week bootcamp where we learned the basics of HTML and CSS, to more advance skills like react and building and hosting our own backend.</p> 
+
+        <p>I am proficient in programming with HTML, CSS, and javascript which I used to connect both the frontend and backend of a website and Database. I have been programming for a few years now manly focusing on frontend. With my skill sets I am able to build a single page website with HTML and CSS or an infinte page website with React and Node.js.</p>
+
+        <p>Currently, I mainly work with the React.js framework which is uses to update and build powerful UI's. I have skills with Node.js so I am able to build basic backend and connect the front and backend together. I have use many diffferent api where I am able to get data and display it show user info or give basic info on products or services. I have used different databases to store data or to retrive data. I mainly use MySQL, but I am also able to use MongoDB, and Localstorage. I have use MySQL for more data heavy projects and MongoDB for more lite projects that only need a small amount of storage. I also use Localstorage on a users broswer to store information like encrypted access codes or usernames and emails.</p>
     </div>
   )
 }
