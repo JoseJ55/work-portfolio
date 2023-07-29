11fdfe2c4e73e8b9f1af27f46a197e19642e46d3
JoseJ55 <josejimenez826@gmail.com>
2023-07-28 20:10:42 -0700

    added new about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,13 +5,13 @@ function About() {
   return (
     <div id="portfolio_about">
         <div id="portfolio_about_shape"></div>
-        <h1>Hello I am Jose Jimenez</h1>
+        <h1>Hello there</h1>
 
-        <p>I am a Web Developer with an Associates’s Degree in computer information science at Victor Valley College and a certificate in Web Development from the University of California Riverside. The certificate from the University of California, Riverside was from a 24-week boot camp where we learned the basics of HTML and CSS, to more advanced skills like react and building and hosting our own backend.</p> 
+        <p>My name is Jose Jimenez, I am a web developer with expertise in both frontend and backend development. My primary focus is on crafting visually stunning interfaces with exceptional usability. I am deeply passionate about staying at the forefront of web development trends, which drives me to continuously expand my skill set.</p> 
 
-        <p>I am proficient in programming with HTML, CSS, and javascript which I used to connect both the frontend and backend of a website and Database. I have been programming for a few years now mainly focusing on the front-end. With my skill sets, I am able to build a single-page website with HTML and CSS or an infinite page website with React and Node.js.</p>
+        <p>In frontend development, I excel in using JavaScript to create captivating animations and leverage popular frameworks like React and Angular to build bespoke components. Additionally, I harness the power of Node.js and Python Django to develop robust and secure backends, enabling me to deliver full-stack projects with confidence.</p>
 
-        <p>Currently, I mainly work with the React.js framework which is used to update and build powerful UI's. I have skills with Node.js so I am able to build a basic backend and connect the front and backend together. I have used many different API where I am able to get data and display it to show user info or give basic info on products or services. I have used different databases to store data or retrieve data. I mainly use MySQL, but I am also able to use MongoDB and Localstorage. I have used MySQL for more data-heavy projects and MongoDB for more lite projects that only need a small amount of storage. I also use Localstorage on a user's browser to store information like encrypted access codes or usernames and emails.</p>
+        <p>Currently, I am actively exploring the realm of 3D modeling and manipulation with Three.js, which adds another dimension of creativity to my repertoire. My commitment to innovation leads me to explore various applications, and I am always eager to acquire new skills that enhance my project capabilities.</p>
     </div>
   )
 }
