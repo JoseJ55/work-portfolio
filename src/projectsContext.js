9e408fd9118d986a81b1cb380ef1454a09428621
JoseJ55 <josejimenez826@gmail.com>
2023-08-11 19:41:03 -0700

    added new pictures

--- src/projectsContext.js
+++ src/projectsContext.js
@@ -6,7 +6,7 @@ export function ProjectsProvider({ children }) {
     const [projects, setProjects] = useState([
         {
             id: 1,
-            title: "Google React Books",
+            title: "Google Books",
             small: "one",
             links: {
                 github: "https://github.com/JoseJ55/google-books", 
@@ -15,7 +15,7 @@ export function ProjectsProvider({ children }) {
             description: "A program that allows the user to search books and either view them at the google estore or save them for later. A user is able to view a whole veriaty of books they can read, with the option of view and buying any book the user wants the user can keep track of the books they want to own. The program utilizes the Google Books API to gather information of the books searched like the title, author(s), and a breif description. The user is also able to save books to the mongoose database for later which there they can also remove from the database when they don't want it anymore. This application uses react and react hooks to make the webpage and make multiple page for every book searched. It also uses node.js and express to gather the data from the google books api and gives data to the frontend when asked with express. Lastly, the books are saved using mongoose to later view them.",
             tools: ["React.js", "React-hooks", "Express.js", "Node.js", "HTML", "CSS", "Axios", "Mongoose"],
             exampleImage: "googleBooksHome.png", // Current doesn't do anything.
-            images: ["/images/googleBooks/googleBooks.png", "/images/googleBooks/googleBooks2.png"],
+            images: ["/images/googleBooks/googleBooks.png", "/images/googleBooks/googleBooksSearch.png"],
             video: "/videos/googleBooks/googleBooksSearchShowcase.mp4",
         },
         {
@@ -29,7 +29,7 @@ export function ProjectsProvider({ children }) {
             description: "A small website using express.js and handlebars engine. The website is mainly used for practice with design and adding suttle animations. Using express to help route and handle the different pages of the website it it fast and easy for user to use. Handlebars was also used it also helps speed up the rendering of the website and maintain the quality of animations and design changes with different screen sizes. The website is also responsive and from 4k to small phone screens it will work with any screen size. Some animations still need to be ploish at certain screen sizes but that is planned for later. A redesign is also in plans for shop page and the trainers page. There are also plans to redesign sections of the website.",
             tools: ["Express.js", "Node.js", "HTML", "CSS", "Javascript"],
             exampleImage: "sunset.jpg",
-            images: ["/images/fitnessGym/fitness101Main.png", "/images/fitnessGym/fitness101main2.png", "/images/fitnessGym/fitness101main3.png", "/images/fitnessGym/fitness101main4.png"],
+            images: ["/images/fitnessGym/fitness101Main.png", "/images/fitnessGym/fitness101main2.png", "/images/fitnessGym/fitness101main3.png", "/images/fitnessGym/fitness101main4.png", "/images/fitnessGym/fitness101main5.png"],
             video: "/videos/teamProfileGenerator/teamProfileGenShowcase.mp4",
         },
         {
@@ -37,8 +37,7 @@ export function ProjectsProvider({ children }) {
             title: "Weather Dashboard",
             small: "three",
             links: {
-                github: "https://github.com/JoseJ55/Weather-bashboard", 
-                live: "https://weather-dash-015.herokuapp.com/"
+                github: "https://github.com/JoseJ55/Weather-bashboard",
             },
             description: "The weather dashboard is an app built using HTML, CSS, and fetch api. A user can search a major city and will recieve weather data on the city they searched for and a simple forcasst for the next five days. The data will be placed in their appropriate location so the user can easily read and look at. The data that will be showen is the tempature, humidity, and wind speed on the top for the current day. The user will also be able to view the tempature, and humidity for the next five days for the city searched. There is also a small search history for the previous 8 cities serached where the user can click on them and show their data for the current day. This application utilizes react and react hooks to create a nice looking webpage. It also uses axios and localstorage to get data from open weather maps api and store that data in the localstorage for later.",
             tools: ["React.js", "React-routes", "React-Hooks", "HTML", "CSS", "Javascript", "Axios", "Localstorage"],
@@ -82,7 +81,7 @@ export function ProjectsProvider({ children }) {
             description: "A small website showing all the planets in the our solar system and other entites that are within our system. Using react to make component and diffrent pages with react hooks. The home page show what the website is and why it was made for, while all the other pages give information on the planet a user has click on for example earth. Each page gives a small fact and a small summary of the planet or information on the planet. The information is currently mainly form https://en.wikipedia.org/wiki/Main_Page. There is a small bit of information from https://www.nasa.gov/ and news articles from the google news api.",
             tools: ["HTML", "CSS", "Javascript", "React.js", "React", "Axios", "Google News API"],
             exampleImage: "solarShowcaseMain.png",
-            images: ["/images/solarShowcase/solarShowcaseMain.png", "/images/solarShowcase/solarShowcaseNav.png", "/images/solarShowcase/solarShowcaseFact.png", "/images/solarShowcase/solarShowcaseAbout.png"]
+            images: ["/images/solarShowcase/solar1.png", "/images/solarShowcase/solar2.png", "/images/solarShowcase/solar3.png", "/images/solarShowcase/solar4.png"]
         }
     ])
 
