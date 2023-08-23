1690aa95814b56fed333d353b57f86aac9dc2ed1
JoseJ55 <josejimenez826@gmail.com>
2023-08-22 18:16:08 -0700

    added animation to about

--- src/new/About/About.jsx
+++ src/new/About/About.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useRef, useState, useMemo, useEffect } from 'react';
 import './About.css';
 
+import AboutText from '../AboutText/AboutText';
+
 function About() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const bodyRef = useRef();
+
+  const observer = useMemo(() => new IntersectionObserver(
+    ([entry]) => {
+      setIsVisible(entry.isIntersecting);
+    }
+  ), []);
+
+  useEffect(() => {
+    observer.observe(bodyRef.current)
+    return () => observer.disconnect()
+  }, [observer])
+
   return (
     <div id='about'>
         <div id='about-profile'>
@@ -11,10 +28,10 @@ function About() {
             </div>
         </div>
 
-        <div id='about-desc'>
-            <p>My name is Jose Jimenez, I am a web developer with expertise in both frontend and backend development. My primary focus is on crafting visually stunning interfaces with exceptional usability. I am deeply passionate about staying at the forefront of web development trends, which drives me to continuously expand my skill set.</p>
-            <p>In frontend development, I excel in using JavaScript to create captivating animations and leverage popular frameworks like React and Angular to build bespoke components. Additionally, I harness the power of Node.js and Python Django to develop robust and secure backends, enabling me to deliver full-stack projects with confidence.</p>
-            <p>Currently, I am actively exploring the realm of 3D modeling and manipulation with Three.js, which adds another dimension of creativity to my repertoire. My commitment to innovation leads me to explore various applications, and I am always eager to acquire new skills that enhance my project capabilities.</p>
+        <div ref={bodyRef} id='about-desc'>
+          <AboutText isVisible={isVisible} time={1} text={'My name is Jose Jimenez, I am a web developer with expertise in both frontend and backend development. My primary focus is on crafting visually stunning interfaces with exceptional usability. I am deeply passionate about staying at the forefront of web development trends, which drives me to continuously expand my skill set.'} />
+          <AboutText isVisible={isVisible} time={2.5} text={'In frontend development, I excel in using JavaScript to create captivating animations and leverage popular frameworks like React and Angular to build bespoke components. Additionally, I harness the power of Node.js and Python Django to develop robust and secure backends, enabling me to deliver full-stack projects with confidence.'} />
+          <AboutText isVisible={isVisible} time={4} text={'Currently, I am actively exploring the realm of 3D modeling and manipulation with Three.js, which adds another dimension of creativity to my repertoire. My commitment to innovation leads me to explore various applications, and I am always eager to acquire new skills that enhance my project capabilities.'} />
         </div>
     </div>
   )
---
+++ src/new/AboutText/AboutText.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useMemo, useRef } from 'react';
+import './AboutText.css';
+
+function AboutText({ isVisible, time, text }) {
+    const [animate, setAnimate] = useState('');
+
+    const paraRef = useRef();
+
+    useMemo(() => {
+        console.log(isVisible);
+        if (isVisible) {
+            setTimeout(() => {
+                setAnimate('slideUp 1s forwards');
+            }, 1000 * time);
+        }
+    }, [isVisible, time])
+
+  return (
+    <div className='about-text'>
+        <p 
+            ref={paraRef} 
+            style={{ animation: animate }}
+        >
+            {text}
+        </p>
+    </div>
+  )
+}
+
+export default AboutText;
