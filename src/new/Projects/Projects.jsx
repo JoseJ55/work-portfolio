ef2a6e7a762f3cc714d2ab0df497db8ab65b5814
JoseJ55 <josejimenez826@gmail.com>
2023-08-27 17:42:50 -0700

    added code for mobile peoject section

--- src/new/Projects/Projects.jsx
+++ src/new/Projects/Projects.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Projects.css';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { set_current_project, set_show_project } from '../../Redux/Slices/Projects';
 
 import ProjectTabs from '../ProjectTabs/ProjectTabs';
 import ProjectImage from '../ProjectImage/ProjectImage';
@@ -9,6 +10,8 @@ import ProjectImage from '../ProjectImage/ProjectImage';
 function Projects() {
 
   const { all } = useSelector((state) => state.projects);
+
+  const dispatch = useDispatch();
 
   return (
     <div id='projects'>
@@ -21,14 +24,53 @@ function Projects() {
 
       <div id='projects-tabs'>
         <ProjectTabs data={all[0]} />
+        <img 
+        className='projects-tabs-image mobile-ony' 
+        onClick={() => {
+          dispatch(set_current_project(all[0].id));
+          dispatch(set_show_project(true));
+        }}
+        src={all[0].images[0]} alt={`Project Sample`} />
         <div className='projects-tabs-break'></div>
+        
         <ProjectTabs data={all[1]} />
+        <img 
+        className='projects-tabs-image mobile-ony'
+        onClick={() => {
+          dispatch(set_current_project(all[1].id));
+          dispatch(set_show_project(true));
+        }} 
+        src={all[1].images[0]} alt={`Project Sample`} />
         <div className='projects-tabs-break'></div>
+        
         <ProjectTabs data={all[2]} />
+        <img 
+        className='projects-tabs-image mobile-ony' 
+        onClick={() => {
+          dispatch(set_current_project(all[2].id));
+          dispatch(set_show_project(true));
+        }}
+        src={all[2].images[0]} alt={`Project Sample`} />
         <div className='projects-tabs-break'></div>
+        
         <ProjectTabs data={all[3]} />
+        <img 
+        className='projects-tabs-image mobile-ony' 
+        onClick={() => {
+          dispatch(set_current_project(all[3].id));
+          dispatch(set_show_project(true));
+        }}
+        src={all[3].images[0]} alt={`Project Sample`} />
         <div className='projects-tabs-break'></div>
+        
         <ProjectTabs data={all[4]} />
+        <img 
+        className='projects-tabs-image mobile-ony' 
+        onClick={() => {
+          dispatch(set_current_project(all[4].id));
+          dispatch(set_show_project(true));
+        }}
+        src={all[4].images[0]} alt={`Project Sample`} />
       </div>
 
       <ProjectImage data={all} />
