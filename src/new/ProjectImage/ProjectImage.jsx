9020f600a58a02d230d28e7c93bd331221791c80
JoseJ55 <josejimenez826@gmail.com>
2023-09-11 15:58:23 -0700

    added border animation on hover and on stand by for project image

--- src/new/ProjectImage/ProjectImage.jsx
+++ src/new/ProjectImage/ProjectImage.jsx
@@ -4,31 +4,82 @@ import './ProjectImage.css';
 import { useSelector } from 'react-redux';
 
 function ProjectImage({ data }) {
+  const { hover_project } = useSelector((state) => state.projects);
 
-    const { current_project, hover_project } = useSelector((state) => state.projects);
+  const [current, setCurrent] = useState(data[0].images[0]);
+  const [isHovering, setIsHovering] = useState(false);
+  const [angle, setAngle] = useState(0);
 
-    const [current, setCurrent] = useState(data[0].images[0]);
+  const handleMouseMove = (event) => {
+    if (isHovering) {
+      const element = event.target;
+      const rect = element.getBoundingClientRect();
 
-    useEffect(() => {
-        const temp = data.filter((project) => {
-            return project.id === hover_project;
-        });
+      const centerX = rect.left + rect.width / 2;
+      const centerY = rect.top + rect.height / 2;
 
-        setCurrent(temp[0].coverImage);
-    }, [hover_project, data])
+      const deltaX = event.clientX - centerX;
+      const deltaY = event.clientY - centerY;
 
-    // useEffect(() => {
-    //     const temp = data.filter((project) => {
-    //         return project.id === current_project;
-    //     });
+      let angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+      angle = (angle + 360) % 360;
 
-    //     setCurrent(temp[0].images[0]);
-    // }, [current_project, data])
+      setAngle(angle);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    const backColor = document.getElementById('projectImage-back');
+
+    requestAnimationFrame(() => {
+      // backColor.style.opacity = 0;
+      backColor.style.animation = 'rotateSelf 5s infinite ease'
+    })
+  }
+
+  useEffect(() => {
+    const backColor = document.getElementById('projectImage-back');
+
+    requestAnimationFrame(() => {
+      backColor.style.opacity = 0.8;
+      backColor.style.transform = `translate(-50%, -50%) rotate(${angle + 90}deg)`;
+      backColor.style.animation = '';
+    })
+  }, [angle])
+
+  useEffect(() => {
+      const temp = data.filter((project) => {
+          return project.id === hover_project;
+      });
+
+      setCurrent(temp[0].coverImage);
+  }, [hover_project, data])
+
+  useEffect(() => {
+    const imgElement = document.getElementById('project-images');
+  
+    const handleMouseOver = () => {
+      setIsHovering(true);
+    };
+  
+    const handleMouseOut = () => {
+      setIsHovering(false);
+    };
+  
+    imgElement.addEventListener('mouseover', handleMouseOver);
+    imgElement.addEventListener('mouseout', handleMouseOut);
+  
+    return () => {
+      imgElement.removeEventListener('mouseover', handleMouseOver);
+      imgElement.removeEventListener('mouseout', handleMouseOut);
+    };
+  }, []);
 
   return (
-    <>
-        <img id='project-images' src={current} alt={`Project Sample`} />
-    </>
+    <div id='projectImage'>
+      <div id='projectImage-back'></div>
+      <img id='project-images' src={current} alt={`Project Sample`} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} />
+    </div>
   )
 }
 
